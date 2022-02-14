acdda9b96fd64fe9ba46ca439f5ed971b81604e1
BorntoRebel <javi412@gmail.com>
2022-02-13 22:48:11 -0800

    implemented webserver and client. Will push more later

---
+++ Milestones/M0/csc648M0/client/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+  <div className="App">
+    <label>Login:</label>
+    <input type="text" />
+    <label>Password:</label>
+    <input type= "text" />
+  </div>
+  );
+}
+
+export default App;

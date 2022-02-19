33066b869e8125ad32b3f0d64f7b6f64167a6f53
tnguyen372 <tnguyen135@mail.sfsu.edu>
2022-02-19 00:51:36 -0800

    trying out wildcard path for express

--- application/Server/index.js
+++ application/Server/index.js
@@ -22,7 +22,7 @@ const db = mysql.createConnection({
 const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
   res.send('Hello World!');
 });

7a0149db681e28b384e8e6daa4a2b56c11986224
Anthony J Souza <ajsouza88@gmail.com>
2022-02-18 19:10:49 -0800

    setting up new path

--- application/ecosystem.config.js
+++ application/ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [{
     name: 'csc648-03-sp22-Team01',
-    cwd:'/home/ubuntu/prod/source',
+    cwd:'/home/ubuntu/prod/source/application/Server',
     script: './application/Server/index.js',
   }],
   deploy: {

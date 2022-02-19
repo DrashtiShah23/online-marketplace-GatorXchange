9a6a376f5ce93904077e4e53106a8ff2efd63e3c
MicheasG <Micky.geta@gmail.com>
2022-02-18 22:15:18 -0800

    Mike update

--- application/client/src/pages/About/AboutMicheas.js
+++ application/client/src/pages/About/AboutMicheas.js
@@ -6,19 +6,24 @@ const AboutMicheas = () => {
 
   return (
     <div className="container">
-      <div>
-      <h1> Micheas </h1>
-      <br />
-      </div>
-      <div>
-        <p>
-         Sample text.
-        </p>
-
-      </div>
+    <div>
+    <h1>Micheas Gebere</h1>
+    <br />
     </div>
-
-
+    <div>
+      <p>
+        Hi there, My name is Micheas Gebere, i am a senior at SFSU graduating in summer 2022 with a Bachlors in Computer science.
+        my time at sfsu has been nothing but fun and exciting skill share expereince. After i graduate, i want to exlore different  
+        options as a software engineer. I plan working in C++ on packaged software but also love front end and back end developments. 
+      </p>
+      <br />
+      <p>
+       In my free time i play and make music, I enjoying composing Ethiopian gospel songs. I am also one of the biggest fan of the alltime 
+       famout sitcom 'FRIENDS' if you mention any scene from it i can probably finish the line for you :). i enjoy watch and playing soccer too. 
+       Thank you for visiting my page. 
+      </p>
+    </div>
+  </div>
   );
 };
 

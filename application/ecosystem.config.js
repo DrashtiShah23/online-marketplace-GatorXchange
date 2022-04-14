aa0bef4a1e6db40e7f737f08401493c5b8c350cf
tnguyen372 <tnguyen135@mail.sfsu.edu>
2022-04-14 04:03:26 -0700

    Testing deployment with db pool and new UI pages

--- application/ecosystem.config.js
+++ application/ecosystem.config.js
@@ -16,9 +16,9 @@ module.exports = {
   deploy: {
     production: {
       user: 'ubuntu',
-      host: '13.57.228.138',
+      host: '3.101.143.240',
       key: '~/.ssh/team1.pem',
-      ref: 'origin/main',
+      ref: 'origin/test-deploy',
       repo: 'git@github.com:CSC-648-SFSU/csc648-03-sp22-Team01.git',
       path: '/home/ubuntu/prod',
       'post-deploy': 'cd ./application/Server && npm install && cd ../ && pm2 reload ecosystem.config.js --env production && pm2 serve ./client/build 3000 --name prod-client'

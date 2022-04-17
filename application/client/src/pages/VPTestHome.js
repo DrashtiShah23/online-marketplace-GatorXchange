abab305facf5f21b3331d32092009cdad3de43b2
tnguyen372 <tnguyen135@mail.sfsu.edu>
2022-04-16 20:56:58 -0700

    Moved searchbar to home page and renders search results on home page

--- application/client/src/pages/VPTestHome.js
+++ application/client/src/pages/VPTestHome.js
@@ -1,24 +1,25 @@
 /*********************************************************************
  * Purpose: Vertical prototype of home page in Milestone 2
  * Input: None
- * Output: Plain text for now. Will be edited later
+ * Output: List of search results based on user search parameters
  * Error Messages: None
  * Author: Thomas Nguyen
  *********************************************************************/
+import SearchBar from '../components/SearchBar';
 import '../css/App.css';
 
 const VPTestHome = () => {
   
   return (
     <div className="container">
-      <header>
-        <h1>Vertical Prototype Test Home Page</h1>
-      </header>
+      
+        <h1><br />Vertical Prototype Test Home Page.<br /><br /></h1>
+        <h2>The platform made for gators, by gators.<br /><br /></h2>
+        <h2>Try using our search bar to browse products.<br /><br /></h2>
+      
       <div>
         <main>
-          <h2>The platform made for gators, by gators.</h2>
-          <br />
-          <h2>Try using our search bar above to browse products.</h2>
+          <SearchBar />
         </main>
       </div>
     </div>

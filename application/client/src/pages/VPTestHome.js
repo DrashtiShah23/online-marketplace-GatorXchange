f3757eae2722b6f86dbe9b2484c01aa417425f3f
WilfredoAceytuno <waceytun@mail.sfsu.edu>
2022-04-25 07:51:59 -0700

    Added products to Homepage

--- application/client/src/pages/VPTestHome.js
+++ application/client/src/pages/VPTestHome.js
@@ -1,33 +1,64 @@
-import React, { useState } from "react";
-import Button from "@restart/ui/esm/Button";
+import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import {Button} from 'react-bootstrap'
 import { Form } from "react-bootstrap";
 import { Modal } from "react-bootstrap";
 import '../css/App.css';
-
 
 const VPTestHome = () => {
   //message box modal
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  let [results, updateResults] = useState([]);
+
+  const displayProductsHome = results.map((result, i) => {
+    return (
+      <div className="card" key={i}>
+        <div className="card-content">
+          <p>Category: {result.category}</p>
+          <p>Image: <img src={result.thumbnail}/></p>
+          <p>Title: {result.title}</p>
+          <p>Price: ${result.price}.00</p>
+          <p>Description: {result.description}</p>
+        </div>
+        <div> 
+          <Button variant="primary" onClick={handleShow}>
+          Contact Seller
+          </Button>
+        </div>
+    </div>
+    )
+  });
+
+  const getAllResultsHome = () => {
+    axios.post("/search",{category: '', searchTerm: ''})
+    axios.get('/search')
+      .then((res) => {
+        results = [...res.data];
+        console.log(results);        
+        updateResults(results);
+      })
+      .catch((err) => {
+        console.log('Failed to get search results' + err);
+      })
+  }
+
+  useEffect(() => {
+    getAllResultsHome();
+  }, []);
   
   return (
     <div className="container">
-      <header>
-        <h1>Vertical Prototype Test Home Page</h1>
-      </header>
-    
-      <div>
-        <main>
-          <h2>The platform made for gators, by gators.</h2>
-          <br />
-          <h2>Try using our search bar above to browse products.</h2>
-
-          {/* "contact seller" pop up sample MODAL */}
       
-      <Button variant="primary" onClick={handleShow}>
-          Contact Seller
-      </Button>
+      <h1>Welcome to GatorXChange <br/> The platform made for gators, by gators.</h1>
+     
+      <div className="card-container">      
+      {displayProductsHome}
+        <main>
+        
+          {/* "contact seller" pop up sample MODAL */}
 
       <Modal show={show} onHide={handleClose}>
           <Modal.Header closeButton>
@@ -67,8 +98,7 @@ const VPTestHome = () => {
             </Button>
           </Modal.Footer>
         </Modal>
-      {/*---------------------*/}
-          
+      {/*---------------------*/} 
         </main>
       </div>
     </div>

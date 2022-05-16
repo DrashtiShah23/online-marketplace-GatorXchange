f6fc769788764a6f474c60ecf353c794bf55ee34
WilfredoAceytuno <waceytun@mail.sfsu.edu>
2022-05-16 11:07:16 -0700

    Fixed post input alignment and made all fields required to post

--- application/client/src/pages/Post.js
+++ application/client/src/pages/Post.js
@@ -8,7 +8,7 @@
  *****************************************************/
 
 import React, { useState } from "react";
-import Form from "react-bootstrap/Form";
+import { Form }from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "../css/post.css";
 import axios from "axios";
@@ -21,6 +21,24 @@ const Post = () => {
   const [val, setVal] = useState();
   const [check, setcheckboxvalue] = useState(false);
   const uploadFileEle = document.getElementById("fileInput");
+
+    // Prevents form from being submitted if form is not valid
+  (function () {
+    var forms = document.querySelectorAll(".needs-validation");
+    Array.prototype.slice.call(forms).forEach(function (form) {
+      form.addEventListener(
+        "submit",
+        function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          form.classList.add("was-validated");
+        },
+        false
+      );
+    });
+  })();
 
   // Event handler for submitting the post information to send to backend
   const handleSubmit = (e) => {
@@ -67,36 +85,40 @@ const Post = () => {
 
   /** Creating a post form where the user enters the details of the post */
   return (
-    <div className="container">
-      <div className="field-Post">
+  <div className="containerPost" >
+    <form
+      className="row row-cols-lg-auto g-3 align-items-center needs-validation"
+      onSubmit={handleSubmit}
+      noValidate
+    >
+      <div className="container">
+        <div className="field">
         <h1> Post an Item </h1>
 
-        <div id="post-label">
-          <Form.Label>Title*: </Form.Label>
-          <div className="post-title">
+        <div className="col-12">
+          <Form.Label className="form-label">Title*: </Form.Label>
             <input
+              className="form-control"
               type="title"
-              placeholder="Title*"
-              onFocus={(e) => (e.target.placeholder = "")}
-              onBlur={(e) => (e.target.placeholder = "Title required!")}
+              placeholder="Title"
               required
               value={title}
               onChange={(e) =>
                 setTitle(e.target.value)
               } /** setting the title by using the event handler e */
             />
-          </div>
+            <div className="invalid-feedback">Title is required</div>
         </div>
-        <div id="post-label">
-          <Form.Label>Price*:</Form.Label>
-          <div className="post-price">
+
+        <div className="col-12">
+          <Form.Label className="form-label">Price*:</Form.Label>
             <input
+              className="form-control"
               value="price"
-              placeholder="Price*"
-              onFocus={(e) => (e.target.placeholder = "")}
-              onBlur={(e) => (e.target.placeholder = "Price required!")}
+              placeholder="Price"
               pattern="[0-9]*"
               value={val}
+              required
               onChange={
                 (e) =>
                   setPrice((v) =>
@@ -104,70 +126,81 @@ const Post = () => {
                   ) /** setting the price by using the event handler e */
               }
             />
-          </div>
+            <div className="invalid-feedback">Price is required</div>
         </div>
-        <div id="post-label">
-          <Form.Label>Description*: </Form.Label>
+
+        <div className="col-12">
+          <Form.Label className="form-label">Description*: </Form.Label>
           <textarea
-            classname="post-input"
-            placeholder="Description*"
+            required
+            className="form-control"
+            placeholder="Description"
             cols="50"
-            rows="2"
-            onFocus={(e) => (e.target.placeholder = "")}
-            onBlur={(e) => (e.target.placeholder = "Description required!")}
+            rows="5"
             required
             value={description}
             onChange={(e) =>
               setDescription(e.target.value)
             } /** setting the description by using the event handler e */
           />
+          <div className="invalid-feedback">Description is required</div>
         </div>
-        <div id="post-label">
-          <Form.Label> Categories* </Form.Label>
-          <Form.Select aria-label="Categories">
-            <option>Select Category*</option>
+
+        <div className="col-12">
+          <Form.Label className="form-label"> Categories* </Form.Label>
+          <Form.Select aria-label="Categories" required className="form-control">
+            <option value="">Select Category</option>
             <option value="Books">Books</option>
             <option value="Electronics">Electronics</option>
             <option value="Clothes">Clothes</option>
           </Form.Select>
+          <div className="invalid-feedback">Category is required</div>
         </div>
-        <div id="post-label">
+
+        <div className="col-12">
           <Form.Label>Location* </Form.Label>
-          <div className="post-pickup">
-            <Form.Select aria-label="Pickup">
-              <option>Select Pickup Location*</option>
+            <Form.Select aria-label="Pickup" required className="form-control">
+              <option value="">Select Pickup Location</option>
               <option value="CesarChavezBldg">Cesar Chavez building </option>
               <option value="Library ">J. Paul Leaonard Library </option>
               <option value="AdminBldg">Administration building</option>
               <option value="CafeRusso">Cafe Russo</option>
               <option value="Quad">Quad</option>
             </Form.Select>
-          </div>
+            <div className="invalid-feedback">Location is required</div>
         </div>
-        <div className="upload-img">
-          <h5>Upload image of the item</h5>
 
+        <div className="upload-img" className="col-12">
+          <h5>Upload image of the item</h5>
           <input
+            className="upload"
+            required
             type="file"
             id="avatar"
             name="avatar"
             accept="image/png, image/jpeg"
           />
+          <div className="invalid-feedback">Image of item is required</div>
         </div>
+
+        <div className="hasAccount col-12">
 
         <p className="post-para">
           The post will be approved by the admin within 24-48 hrs after the
           upload
         </p>
-        <button className="postButton" onSubmit={handleSubmit}>
+        <button className="postButton" type="submit">
           Post
         </button>
         {/* Having a cancel link so users can be directed to home page */}
-        <Link className="cancel-link" to="/" c>
+        <Link className="cancel-link" to="/" >
           Cancel
         </Link>
-      </div>
-    </div>
+        </div>
+        </div>
+      </div> 
+    </form>
+  </div>
   );
 };
 
